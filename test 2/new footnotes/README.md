9a5a1289c768dcd57c280107cd926d1847234871
Umaid Shahid <umaidshahid1@gmail.com>
2024-05-15 17:26:11 +0500

    aria to be implemented yet

---
+++ test 2/new footnotes/README.md
@@ -0,0 +1,3 @@
+# helleyh TinyMCE Plugin
+
+Welcome stranger! This is a repo containing the helleyh TinyMCE plugin.
---
+++ test 2/new footnotes/src/main/ts/Main.ts
@@ -0,0 +1,9 @@
+import Plugin from './Plugin';
+
+Plugin();
+
+/** *****
+ * DO NOT EXPORT ANYTHING
+ *
+ * IF YOU DO ROLLUP WILL LEAVE A `helleyh` GLOBAL ON THE PAGE
+ *******/
