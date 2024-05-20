2d2d9c3f62f7752e8e11f1697bb5a0d79958566d
Umaid Shahid <umaidshahid1@gmail.com>
2024-05-21 01:30:26 +0500

    new directory with renamed files from new-footnotes to ally

---
+++ a11y-footnotes/README.md
@@ -0,0 +1,3 @@
+# a11yFootnotes TinyMCE Plugin
+
+Welcome stranger! This is a repo containing the a11yFootnotes TinyMCE plugin.
---
+++ a11y-footnotes/scratch/compiled/plugin.js
@@ -0,0 +1,98 @@
+(function () {
+    'use strict';
+
+    const setup = (editor, url) => {
+      let footnoteCounter = 1;
+      const css = `
+        .footnote-highlight {
+            background-color: yellow !important;
+        }
+    `;
+      const style = document.createElement('style');
+      style.type = 'text/css';
+      style.appendChild(document.createTextNode(css));
+      document.head.appendChild(style);
+      editor.ui.registry.addButton('a11y-footnotes', {
+        text: 'Footnote',
+        onAction: () => {
+          const selectedText = editor.selection.getContent({ format: 'text' });
+          if (!selectedText) {
+            editor.windowManager.alert('Please select a word or text to add a footnote.');
+            return;
+          }
+          editor.windowManager.open({
+            title: 'Insert Footnote',
+            body: {
+              type: 'panel',
+              items: [{
+                  type: 'input',
+                  name: 'footnoteText',
+                  label: 'Footnote Text'
+                }]
+            },
+            buttons: [
+              {
+                type: 'cancel',
+                text: 'Cancel'
+              },
+              {
+                type: 'submit',
+                text: 'Save',
+                primary: true
+              }
+            ],
+            onSubmit: function (api) {
+              const data = api.getData();
+              insertFootnote(selectedText, data.footnoteText);
+              api.close();
+            }
+          });
+        }
+      });
+      function insertFootnote(selectedText, footnoteText) {
+        const footnoteId = 'footnote-' + new Date().getTime();
+        const footnoteNumber = footnoteCounter++;
+        const footnoteMarker = `<span>${ selectedText }<a id="footnote-entry-${ footnoteId }-ref" role="doc-noteref" aria-labelledby="footnote-entry-${ footnoteId }" href="#footnote-entry-${ footnoteId }"><sup>[${ footnoteNumber }]</sup></a></span>`;
+        const footnoteContent = `
+            <li id="footnote-entry-${ footnoteId }">
+                <a id="footnote-entry-${ footnoteId }">
+                    
+                </a>
+                ${ footnoteText }
+                <a role="doc-backlink" href="#footnote-entry-${ footnoteId }-ref" aria-label="Back to content">\u21b5</a>
+            </li>`;
+        editor.insertContent(footnoteMarker);
+        const footnotesSection = editor.dom.select('section[role="doc-footnotes"] ol')[0];
+        if (footnotesSection) {
+          footnotesSection.innerHTML += footnoteContent;
+        } else {
+          editor.setContent(editor.getContent() + `<hr /><section role="doc-footnotes" aria-label="footnotes">
+                <h3 class="sr-only" id="footnotes-block-${ footnoteId }">Footnotes</h3>
+                <ol>${ footnoteContent.trim() }</ol>
+            </section>`);
+        }
+      }
+      function removeOrphanFootnotes() {
+        const footnoteRefs = editor.dom.select('a[role="doc-noteref"]');
+        const footnoteIds = footnoteRefs.map(ref => ref.id.replace('-ref', ''));
+        const footnotes = editor.dom.select('li[id^="footnote-"]');
+        footnotes.forEach(footnote => {
+          const footnoteId = footnote.id;
+          if (!footnoteIds.includes(footnoteId)) {
+            editor.dom.remove(footnote);
+          }
+        });
+        const footnotesSection = editor.dom.select('section[role="doc-footnotes"]')[0];
+        if (footnotesSection && footnotesSection.querySelectorAll('li').length === 0) {
+          editor.dom.remove(footnotesSection);
+        }
+      }
+      editor.on('input', removeOrphanFootnotes);
+    };
+    var Plugin = () => {
+      tinymce.PluginManager.add('a11y-footnotes', setup);
+    };
+
+    Plugin();
+
+})();
